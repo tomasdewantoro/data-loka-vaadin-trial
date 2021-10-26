79561fdec0a543dcf8ab21108c0245b7cb3f5317
Marcus Hellberg <marcus@vaadin.com>
2021-10-26 16:52:00 -0700

    remove broken imports

--- frontend/views/fusion/fusion-view.ts
+++ frontend/views/fusion/fusion-view.ts
@@ -3,12 +3,9 @@ import { GridActiveItemChangedEvent } from '@vaadin/grid';
 import '@vaadin/text-field';
 import '@vaadin/email-field';
 import '@vaadin/button';
-import Person from 'Frontend/generated/com/example/application/data/entity/Person';
-import { PersonService } from 'Frontend/generated/endpoints';
 import { html } from 'lit';
 import { customElement, state } from 'lit/decorators.js';
 import { View } from '../view';
-import PersonModel from 'Frontend/generated/com/example/application/data/entity/PersonModel';
 import { Binder, field } from '@vaadin/form';
 
 @customElement('fusion-view')
