9e63fe9df6abe8b4924c359fa505d4e512d9f93b
Marcus Hellberg <marcus@vaadin.com>
2021-10-27 13:47:13 -0700

    remove phone generation

--- src/main/java/com/example/application/data/generator/DataGenerator.java
+++ src/main/java/com/example/application/data/generator/DataGenerator.java
@@ -34,7 +34,6 @@ public class DataGenerator {
             personRepositoryGenerator.setData(Person::setFirstName, DataType.FIRST_NAME);
             personRepositoryGenerator.setData(Person::setLastName, DataType.LAST_NAME);
             personRepositoryGenerator.setData(Person::setEmail, DataType.EMAIL);
-            personRepositoryGenerator.setData(Person::setPhone, DataType.PHONE_NUMBER);
             personRepositoryGenerator.setData(Person::setDateOfBirth, DataType.DATE_OF_BIRTH);
             personRepository.saveAll(personRepositoryGenerator.create(100, seed));
 
